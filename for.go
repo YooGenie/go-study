01d4747061c22d901fa22060ca302cc2595e3696
ysun7 <ysun7@beautifulstore.org>
2022-02-12 22:54:58 +0900

    #15 for문 공부하기

---
+++ for.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func StudyFor() {
+	// 중첩 for 문 예시
+	for i := 0; i < 5; i++ {
+		for j := 0; j < i+1; j++ {
+			fmt.Print("*")
+		}
+		fmt.Println()
+	}
+
+	// continue 예시
+	for i := 0; i < 5; i++ {
+		if i == 0 {
+			fmt.Println("i=0입니다.")
+			continue
+		}
+		fmt.Println(i)
+	}
+
+	// break 예시
+	for i := 0; i < 5; i++ {
+		fmt.Println(i)
+		if i == 0 {
+			fmt.Println("i=0입니다.")
+			break
+		}
+	}
+}

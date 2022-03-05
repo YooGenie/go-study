b350bf171fa1400626ef1221b0703bdd8adadba1
ysun7 <ysun7@beautifulstore.org>
2022-03-05 22:26:12 +0900

    #27 숫자 랜덤으로 생성하기

--- main.go
+++ main.go
@@ -1,27 +1,25 @@
 package main
 
 import (
-	"fmt"
-	"github.com/YooGenie/go-study/basic"
-	"github.com/YooGenie/go-study/compare"
 	"github.com/YooGenie/go-study/function"
 )
 
 func main() {
-	compare.CompareIfWithSwitch(5)
-	fmt.Println("")
-	function.Trim("          유지니             ")
-	fmt.Println("")
-	basic.SetStruct()
-	fmt.Println("")
-	basic.EmbeddedField()
-	fmt.Println("")
-	basic.StudyIf("red", 29)
-	basic.StudySwitch("sunday",28)
-	basic.StudyFor()
-	basic.StudyForRange()
-	basic.StudyArray()
-	function.CheckType()
-	basic.SliceStudy()
-	function.FillZero(12365)
+	//compare.CompareIfWithSwitch(5)
+	//fmt.Println("")
+	//function.Trim("          유지니             ")
+	//fmt.Println("")
+	//basic.SetStruct()
+	//fmt.Println("")
+	//basic.EmbeddedField()
+	//fmt.Println("")
+	//basic.StudyIf("red", 29)
+	//basic.StudySwitch("sunday",28)
+	//basic.StudyFor()
+	//basic.StudyForRange()
+	//basic.StudyArray()
+	//function.CheckType()
+	//basic.SliceStudy()
+	//function.FillZero(12365)
+	function.Random()
 }

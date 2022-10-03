9e8df0f069c439c9eb56d9e0d2d0d6bab16bc923
YooGenie <yshee712@gmail.com>
2022-10-03 21:32:50 +0900

    #42 채널에서 blocking 공부

--- basic/channel.go
+++ basic/channel.go
@@ -21,4 +21,45 @@ func Channel() {
 	fmt.Println(received1)
 	fmt.Println(received2)
 
+	BlockCaseFull()
+	BlockCaseEmpty()
+}
+
+func BlockCaseFull() {
+	/*
+		1,2,3 넣으면 채널이 꽉 찼다. 이 상태에서 4를 넣으면 어떻게 될까?
+		=> 채널이 꽉 차서 4는 send하기 위해서 계속 대기를 탄다.
+		=> finish를 출력하지 못하고 프로그램이 비정상적으로 종료 된다
+		=> 아래와 같은 메시지가 뜬다
+		=> fatal error: all goroutines are asleep - deadlock!
+	*/
+	ch := make(chan int, 3)
+	ch <- 1
+	ch <- 2
+	ch <- 3
+	ch <- 4
+
+	fmt.Println("finish")
+}
+
+func BlockCaseEmpty() {
+	/*
+		1,2,3를 넣고 receive를 4번 하면 어떻게 될까?
+		=> 4번째에서 채널안에 값이 없어서 가져올 수 없는 상황이다.
+		=> 그래서 무한 대기를 타다가 비장성적으로 종료가 된다
+		=> 아래와 같은 메시지가 뜬다
+		=> fatal error: all goroutines are asleep - deadlock!
+	*/
+	ch := make(chan int, 3)
+	ch <- 1
+	ch <- 2
+	ch <- 3
+
+	<-ch         // 값을 꺼내지만 사용하지 않는 경우 이다.
+	num1 := <-ch //값을 꺼내서 변수에 넣어주는 경우
+	fmt.Println(num1)
+	<-ch
+	<-ch //채널에 값을 가져올 수 없어서 무한 대기
+
+	fmt.Println("finish")
 }

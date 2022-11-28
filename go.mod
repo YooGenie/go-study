0333b2e09af7d09b64d15f3c9187d64e02812275
sunny <yshee712@gmail.com>
2022-11-28 11:05:30 +0900

    예시

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/YooGenie/go-study
 go 1.16
 
 require (
-	github.com/alibaba/sentinel-golang v1.0.4 // indirect
-	github.com/google/uuid v1.3.0 // indirect
+	github.com/alibaba/sentinel-golang v1.0.4
+	github.com/beautiful-store/grpc v1.0.13
+	google.golang.org/genproto v0.0.0-20221118155620-16455021b5e6 // indirect
+	google.golang.org/grpc v1.51.0
 )
--- main.go
+++ main.go
@@ -1,8 +1,49 @@
+//package main
+//
+//import "github.com/YooGenie/go-study/basic"
+//
+//func main() {
+//	//compare.CompareIfWithSwitch(5)
+//	//fmt.Println("")
+//	//function.Trim("          유지니             ")
+//	//fmt.Println("")
+//	//basic.SetStruct()
+//	//fmt.Println("")
+//	//basic.EmbeddedField()
+//	//fmt.Println("")
+//	//basic.StudyIf("red", 29)
+//	//basic.StudySwitch("sunday",28)
+//	//basic.StudyFor()
+//	//basic.StudyForRange()
+//	//basic.StudyArray()
+//	//function.CheckType()
+//	//basic.SliceStudy()
+//	//function.FillZero(12365)
+//	//function.Random()
+//	//function.SliceStringAndConvertInt()
+//	//basic.Map()
+//	//function.ListAndMap()
+//	//function.Join()
+//	//basic.Channel()
+//	//basic.SolutionOne()
+//	//basic.SolutionTwo()
+//	//basic.StudyWithTimeout()
+//	basic.StringToStruct()
+//}
+
 package main
 
-import "github.com/YooGenie/go-study/basic"
+import (
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+)
+
+const portNumber = "9000"
 
 func main() {
+<<<<<<< Updated upstream
 	//compare.CompareIfWithSwitch(5)
 	//fmt.Println("")
 	//function.Trim("          유지니             ")
@@ -29,4 +70,17 @@ func main() {
 	//basic.SolutionTwo()
 	//basic.StudyWithTimeout()
 	basic.StudyWithDeadline()
+=======
+	lis, err := net.Listen("tcp", ":"+portNumber)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	grpcServer := grpc.NewServer()
+
+	log.Printf("start gRPC server on %s port", portNumber)
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %s", err)
+	}
+>>>>>>> Stashed changes
 }
